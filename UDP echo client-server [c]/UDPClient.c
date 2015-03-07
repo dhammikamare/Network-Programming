f3e1b836fa3043ecba19ebdbaca3c3703c0d7627
Dhammika Marasinghe <dhammikammdb123@gmail.com>
2015-03-08 00:59:07 +0530

    UDP echo server added

--- UDP echo client-server [c]/UDPClient.c
+++ UDP echo client-server [c]/UDPClient.c
@@ -1,6 +1,9 @@
-/* Sample UDP client 
+/* 
+ * UDP client 
  * @author: Department of Computer Engineering, University of Peradeniya
+ * @modified-by: Dhammika Marasinghe | https://github.com/dhammika-marasinghe
  */
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -16,14 +19,22 @@ int main(int argc, char**argv) {
         printf("usage:%s <IP address>\n", argv[0]);
         return -1;
     }
+
+    // Create a socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     bzero(&servaddr, sizeof (servaddr));
+
+    // Initialize a sockaddr_in struct with the information of the server intended to connect to
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = inet_addr(argv[1]);
     servaddr.sin_port = htons(32000);
-    sendto(sockfd, sendline, strlen(sendline), 0, (struct sockaddr *) &servaddr, sizeof (servaddr));
+
+    // send and receive messages
+    sendto(sockfd, sendline, strlen(sendline), 0, (struct sockaddr*) &servaddr, sizeof (servaddr));
+
     n = recvfrom(sockfd, recvline, 10000, 0, NULL, NULL);
     recvline[n] = 0;
-    printf("Received: %s\n", recvline);
+    printf("C: Received: %s\n", recvline);
+
     return 0;
 }
