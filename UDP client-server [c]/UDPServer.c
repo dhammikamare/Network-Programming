cb67e46d3588f104d8d9adec326d968c2f6735e0
Dhammika Marasinghe <dhammikammdb123@gmail.com>
2015-03-08 00:50:52 +0530

    UDP client-server updated

--- UDP client-server [c]/UDPServer.c
+++ UDP client-server [c]/UDPServer.c
@@ -1,6 +1,9 @@
-/* Sample UDP server 
+/* 
+ * UDP server 
  * @author: Department of Computer Engineering, University of Peradeniya
+ * @modified-by: Dhammika Marasinghe | https://github.com/dhammika-marasinghe
  */
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <strings.h>
@@ -12,15 +15,27 @@ int main(int argc, char**argv) {
     socklen_t len;
     char mesg[1000];
     char* banner = "Hello UDP client! This is UDP server";
+
+    // create a socket with appropriate options for UDP.
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    // initialize a sockaddr_in struct with appropriate options for our server address.
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(32000);
+
+    // bind the socket with the created sockaddr_in struct.
     bind(sockfd, (struct sockaddr *) &servaddr, sizeof (servaddr));
-    len = sizeof (cliaddr);
-    n = recvfrom(sockfd, mesg, 1000, 0, (struct sockaddr*) &cliaddr, &len);
-    sendto(sockfd, banner, n, 0, (struct sockaddr *) &cliaddr, sizeof (cliaddr));
-    mesg[n] = 0;
-    printf("Received: %s\n", mesg);
+
+    // indefinitely, replying for all clients until it is forced to terminate by the host computer.
+    while (1) {
+        // send and receive messages from clients that sockaddr_in struct is configured to use.
+        len = sizeof (cliaddr);
+        n = recvfrom(sockfd, mesg, 1000, 0, (struct sockaddr *) &cliaddr, &len);
+        mesg[n] = 0;
+        printf("Received: %s\n", mesg);
+
+        sendto(sockfd, banner, n, 0, (struct sockaddr *) &cliaddr, sizeof (cliaddr));
+    }
     return 0;
 }
