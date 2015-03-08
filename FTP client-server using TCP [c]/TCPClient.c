dbb5dbfb8c37e28aa7b7015401c29ce64c33b705
Dhammika Marasinghe <dhammikammdb123@gmail.com>
2015-03-08 09:51:16 +0530

    FTP client-server using TCP [c] added

--- FTP client-server using TCP [c]/TCPClient.c
+++ FTP client-server using TCP [c]/TCPClient.c
@@ -1,6 +1,14 @@
 /* 
- * Sample TCP client 
+ * TCP client for FTP
  * @author: Department of Computer Engineering, University of Peradeniya
+ * @modified-by: Dhammika Marasinghe | https://github.com/dhammika-marasinghe
+ * @task: A server similar to an FTP server which sends a file over a network using a TCP connection along with its client.
+ *      The client and the server follow the following protocol:
+ *      The client connects and sends 'request' string, requesting the file '<file_mame>' from the server.
+ *      Server responds with the size of the file.
+ *      Server then sits in a loop, sending the file, 1000 bytes at a time. 
+ *      Upon receiving each 1000 bytes, the client appends the received part to a file called '<file_mame>'. 
+ *      The client disconnects upon receiving the full file. 
  */
 
 #include <sys/socket.h>
@@ -9,14 +17,18 @@
 #include <stdio.h>
 #include <string.h>
 
+/* no. of bytes per part */
+#define SIZE 1000
+
 int main(int argc, char**argv) {
-    int sockfd, n;
+    FILE *downFile;
     struct sockaddr_in servaddr;
-    char banner[] = "Hello TCP server! This is TCP client";
-    char buffer[1000];
-    
-    if (argc != 2) {
-        printf("usage: ./%s <IP address>\n", argv[0]);
+    char downFileName[255];
+    char buffer[SIZE];
+    int sockfd, n, serves, i;
+
+    if (argc != 3) {
+        printf("usage: ./%s <ip_address> <file_name>\n", argv[0]);
         return -1;
     }
 
@@ -31,11 +43,33 @@ int main(int argc, char**argv) {
     /* connect to the server. */
     connect(sockfd, (struct sockaddr *) &servaddr, sizeof (servaddr));
 
-    sendto(sockfd, banner, strlen(banner), 0, (struct sockaddr *) &servaddr, sizeof (servaddr));
+    /* send request */
+    printf("request file: %s\n", argv[2]);
+    sendto(sockfd, argv[2], strlen(argv[2]), 0, (struct sockaddr *) &servaddr, sizeof (servaddr));
 
-    n = recvfrom(sockfd, buffer, 10000, 0, NULL, NULL);
+    /* get number of parts */
+    n = recvfrom(sockfd, buffer, sizeof (int), 0, NULL, NULL);
     buffer[n] = 0;
-    printf("Received:%s\n", buffer);
+    sscanf(buffer, "%d", &serves);
+    printf("getting %s as %d serves.\n", argv[2], serves);
+
+    /* open file to append */
+    sprintf(downFileName, "_%s", argv[2]);
+    downFile = fopen(downFileName, "a");
+    if (downFile == NULL) {
+        printf("error opening file.\n");
+    }
+    /* download as parts */
+    for (i = 1; i <= serves; i++) {
+        //printf("getting part %d\n", i);
+        n = recvfrom(sockfd, buffer, SIZE, 0, NULL, NULL);
+        buffer[n] = 0;
+        fprintf(downFile, "%s", buffer);
+        //printf("received: %s\n", buffer);
+    }
+    /* finish downloading */
+    fclose(downFile);
+    printf("%s file successfully downloaded. :) \n", argv[2]);
 
     return 0;
 }
--- FTP client-server using TCP [c]/TCPServer.c
+++ FTP client-server using TCP [c]/TCPServer.c
@@ -1,19 +1,32 @@
 /* 
- * Sample TCP server 
+ * TCP server for FTP
  * @author: Department of Computer Engineering, University of Peradeniya
+ * @modified-by: Dhammika Marasinghe | https://github.com/dhammika-marasinghe
+ * @task: A server similar to an FTP server which sends a file over a network using a TCP connection along with its client.
+ *      The client and the server follow the following protocol:
+ *      The client connects and sends 'request' string, requesting the file '<file_mame>' from the server.
+ *      Server responds with the size of the file.
+ *      Server then sits in a loop, sending the file, 1000 bytes at a time. 
+ *      Upon receiving each 1000 bytes, the client appends the received part to a file called '<file_mame>'. 
+ *      The client disconnects upon receiving the full file. 
  */
 
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+/* no. of bytes per part */
+#define SIZE 1000
 
 int main(int argc, char**argv) {
-    int listenfd, connfd, n;
+    FILE *reqFile;
     struct sockaddr_in servaddr, cliaddr;
     socklen_t clilen;
-    char* banner = "Hello TCP client! This is TCP server";
-    char buffer[1000];
+    char buffer[SIZE];
+    int listenfd, connfd, n, index = 1, i = 0, chr;
 
     /* one socket is dedicated to listening */
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -32,12 +45,51 @@ int main(int argc, char**argv) {
     clilen = sizeof (cliaddr);
 
     /* accept the client with a different socket. */
-    connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen);
-    // the uninitialized cliaddr variable is filled in with the information of the client by recvfrom function
-    n = recvfrom(connfd, buffer, 1000, 0, (struct sockaddr *) &cliaddr, &clilen);
-    buffer[n] = 0;
+    connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen); // the uninitialized cliaddr variable is filled in with the
 
-    sendto(connfd, banner, strlen(banner), 0, (struct sockaddr *) &cliaddr, sizeof (cliaddr));
-    printf("Received:%s\n", buffer);
+    /* getting client request */
+    n = recvfrom(connfd, buffer, SIZE, 0, (struct sockaddr *) &cliaddr, &clilen); // information of the client by recvfrom function
+    buffer[n] = 0;
+    printf("requested:%s\n", buffer);
+
+    /* open requested file */
+    reqFile = fopen(buffer, "r");
+    if (reqFile == NULL) {
+        char* errorMsg = "error opening file.\n";
+        printf("%s", errorMsg);
+        // sendto(connfd, errorMsg, strlen(errorMsg), 0, (struct sockaddr *) &cliaddr, sizeof (cliaddr));
+    }
+
+    /* find file length */
+    fseek(reqFile, 0, 2);
+    long int size = ftell(reqFile);
+    printf("%s size: %ld\n", buffer, size);
+
+    /* calculate no. of serves and send it to client */
+    int serves = (size / SIZE) + 1;
+    printf("serves: %d\n", serves);
+    sprintf(buffer, "%d\n", serves);
+    sendto(connfd, buffer, sizeof (int), 0, (struct sockaddr *) &cliaddr, sizeof (cliaddr));
+
+    rewind(reqFile);
+
+    /* sending file to client */
+    while (1) {
+        chr = fgetc(reqFile);
+        buffer[i++] = chr;
+        if (chr == EOF) { // last part of file
+            buffer[--i] = 0;
+            //printf("sending: %s\n", buffer);
+            sendto(connfd, buffer, strlen(buffer), 0, (struct sockaddr *) &cliaddr, sizeof (cliaddr));
+            break;
+        }
+        if (ftell(reqFile) == (index * SIZE)) { // send by part
+            //printf("sending: %s\n", buffer);
+            sendto(connfd, buffer, strlen(buffer), 0, (struct sockaddr *) &cliaddr, sizeof (cliaddr));
+            i = 0;
+        }
+    }
+    fclose(reqFile);
+
     return 0;
 }
